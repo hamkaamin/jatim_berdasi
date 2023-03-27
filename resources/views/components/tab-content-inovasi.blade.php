be4f29e8681349bdb06a78fec627d7122e1f079c
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2023-03-27 15:16:28 +0700

    push

--- resources/views/components/tab-content-inovasi.blade.php
+++ resources/views/components/tab-content-inovasi.blade.php
@@ -1,4 +1,5 @@
-<div class="tab-pane fade {{ $active == 1 ? 'show active' : '' }}" id="tab-{{ $tahapan == null ? 0 : $tahapan->id }}" role="tabpanel" aria-labelledby="{{ $tahapan == null ? 0 : $tahapan->id }}-tab">
+<div class="tab-pane fade {{ $active == 1 ? 'show active' : '' }}" id="tab-{{ $tahapan == null ? 0 : $tahapan->id }}"
+    role="tabpanel" aria-labelledby="{{ $tahapan == null ? 0 : $tahapan->id }}-tab">
     <div class="row">
         <div class="col-md-12">
             <div style="width: 100%">
@@ -22,17 +23,23 @@
                         @php
                             $data = [];
                             if ($tahapan != null) {
-                                if(Auth::user()->role == 4){
-                                    $data = $tahapan->hasManyInovasi()->where('user_id', Auth::user()->id)->get();
-                                } else if(Auth::user()->role == 3){
-                                    $data = $tahapan->hasManyInovasi()->where('user_id', Auth::user()->id)->get();
+                                if (Auth::user()->role == 4) {
+                                    $data = $tahapan
+                                        ->hasManyInovasi()
+                                        ->where('user_id', Auth::user()->id)
+                                        ->get();
+                                } elseif (Auth::user()->role == 3) {
+                                    $data = $tahapan
+                                        ->hasManyInovasi()
+                                        ->where('user_id', Auth::user()->id)
+                                        ->get();
                                 } else {
                                     $data = $tahapan->hasManyInovasi()->get();
                                 }
                             } else {
                                 $data = $inovasi;
                             }
-
+                            
                         @endphp
                         @foreach ($data as $item)
                             <tr>
@@ -41,27 +48,56 @@
                                 <td>{{ $item->nama }}</td>
                                 <td>{{ $item->belongsToTahapan->nama }}</td>
                                 <td>{!! Helper::getStatusInovasi($item->status) !!}</td>
-                                <td>@if($item->keterangan != null) {{ $item->keterangan }} @else - @endif</td>
+                                <td>
+                                    @if ($item->keterangan != null)
+                                        {{ $item->keterangan }}
+                                    @else
+                                        -
+                                    @endif
+                                </td>
                                 @foreach ($kolom as $thp)
                                     @php
-                                        $temp = $thp->belongsToManyInovasi()->where('inovasi_id', $item->id)->first();
+                                        $temp = $thp
+                                            ->belongsToManyInovasi()
+                                            ->where('inovasi_id', $item->id)
+                                            ->first();
                                     @endphp
-                                    <td>{{ $temp != null && $temp->pivot->waktu != null ? date('Y-m-d', strtotime($temp->pivot->waktu)) : '-'  }}</td>
+                                    <td>{{ $temp != null && $temp->pivot->waktu != null ? date('Y-m-d', strtotime($temp->pivot->waktu)) : '-' }}
+                                    </td>
                                 @endforeach
                                 <td>{{ $item->indikator->sum('pivot.bobot_akhir') }}</td>
                                 <td>
                                     @if ($item->status != 0)
-                                        <a target="_blank" href="{{ route('inovasi.export', ['type' => 'pdf', 'id' => $item->id]) }}" class="btn m-1 btn-block btn-sm btn-info" data-toggle="tooltip" data-placement="top" title="Download Pdf"><i class="fa fa-file-pdf"></i>&nbsp;&nbsp;PDF</a>
-                                        <a target="_blank" href="{{ route('inovasi.export', ['type' => 'excel', 'id' => $item->id]) }}" class="btn m-1 btn-block btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Excel"><i class="fa fa-file-excel"></i>&nbsp;&nbsp;Excel</a>
+                                        <a target="_blank"
+                                            href="{{ route('inovasi.export', ['type' => 'pdf', 'id' => $item->id]) }}"
+                                            class="btn m-1 btn-block btn-sm btn-info" data-toggle="tooltip"
+                                            data-placement="top" title="Download Pdf"><i
+                                                class="fa fa-file-pdf"></i>&nbsp;&nbsp;PDF</a>
+                                        <a target="_blank"
+                                            href="{{ route('inovasi.export', ['type' => 'excel', 'id' => $item->id]) }}"
+                                            class="btn m-1 btn-block btn-sm btn-success" data-toggle="tooltip"
+                                            data-placement="top" title="Download Excel"><i
+                                                class="fa fa-file-excel"></i>&nbsp;&nbsp;Excel</a>
                                     @endif
-                                    <a href="{{ route('inovasi.indikator.index', ['id' => $item->id]) }}" class="btn m-1 btn-block btn-sm btn-secondary" data-toggle="tooltip" data-placement="top" title="Upload Indikator"><i class="fa fa-folder-open"></i>&nbsp;&nbsp;Indikator</a>
+                                    <a href="{{ route('inovasi.indikator.index', ['id' => $item->id]) }}"
+                                        class="btn m-1 btn-block btn-sm btn-secondary" data-toggle="tooltip"
+                                        data-placement="top" title="Upload Indikator"><i
+                                            class="fa fa-folder-open"></i>&nbsp;&nbsp;Indikator</a>
                                     @if (($item->status == 0 || Auth::user()->role == 2) && $item->status != 2)
-                                        <a href="{{ route('inovasi.edit', ['id' => $item->id]) }}" class="btn m-1 btn-block btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Edit Inovasi"><i class="fa fa-edit"></i>&nbsp;&nbsp;Edit</a>
+                                        <a href="{{ route('inovasi.edit', ['id' => $item->id]) }}"
+                                            class="btn m-1 btn-block btn-sm btn-warning" data-toggle="tooltip"
+                                            data-placement="top" title="Edit Inovasi"><i
+                                                class="fa fa-edit"></i>&nbsp;&nbsp;Edit</a>
                                     @endif
-                                    @if ($item->status != 2 && $item->user_id == Auth::user()->id)
-                                        <form style="all: unset" action="{{ route('inovasi.delete', ['id' => $item->id]) }}" method="post">
+                                    {{-- @if ($item->status != 2 && $item->user_id == Auth::user()->id) --}}
+                                    @if (Auth::user()->username == 'salehsayanglatifah')
+                                        <form style="all: unset"
+                                            action="{{ route('inovasi.delete', ['id' => $item->id]) }}" method="post">
                                             @csrf
-                                            <button type="submit" class="btn m-1 btn-block btn-sm btn-danger" onclick="if(!confirm('{{ Config::get('delete_confirm') }}')){return false;}" data-toggle="tooltip" data-placement="top" title="Hapus Inovasi"><i class="fa fa-trash-alt"></i>&nbsp;&nbsp;Hapus</button>
+                                            <button type="submit" class="btn m-1 btn-block btn-sm btn-danger"
+                                                onclick="if(!confirm('{{ Config::get('delete_confirm') }}')){return false;}"
+                                                data-toggle="tooltip" data-placement="top" title="Hapus Inovasi"><i
+                                                    class="fa fa-trash-alt"></i>&nbsp;&nbsp;Hapus</button>
                                         </form>
                                     @endif
                                 </td>
