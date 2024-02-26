f9a589bace2a1464f0657c996bceb2d6894aa134
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-02-26 11:25:11 +0700

    push jatim berdasi

--- app/Http/Controllers/IndikatorController.php
+++ app/Http/Controllers/IndikatorController.php
@@ -17,7 +17,7 @@ class IndikatorController extends Controller
         $indikator_inovasi = Indikator::where('label', 0)->get();
         // dd($indikator_inovasi);
         $indikator_provinsi = Indikator::where('label', 1)->get();
-        $data_kategori = KategoriInovasi::all();
+        $data_kategori = KategoriInovasi::orderBy('id','asc')->get();
         return view('master.indikator', compact('indikator_inovasi', 'indikator_provinsi','data_kategori'));
     }
 
