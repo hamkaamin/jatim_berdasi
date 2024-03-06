56d1aa73a52c63594eafab6e286ec9be2efdc8d9
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-03-06 18:34:02 +0700

    pushh api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -171,7 +171,7 @@ class ApiController extends Controller
     public function insert_inovasi2(Request $request)
     {
         $arr_data = $request->arr_data;
-        $arr_data = json_encode($arr_data);
+        // $arr_data = json_encode($arr_data);
         $arr_data = json_decode($arr_data,true);
         foreach($arr_data as $item)
         {
