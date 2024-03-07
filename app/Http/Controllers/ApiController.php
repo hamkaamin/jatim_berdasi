6cc00af6671d2da37b9df3990e90c73b1a060150
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-03-07 10:01:45 +0700

    pushh api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -174,16 +174,17 @@ class ApiController extends Controller
         $arr_data = $request->arr_data;
         // $arr_data = json_encode($arr_data);
         $arr_data = json_decode($arr_data,true);
-        return response()->json([
-            'status' => true,
-            'message' => "All Data Inovasi!",
-            'data' => $arr_data
-        ], 200);
+        
         foreach($arr_data as $item)
         {
             $data_inovasi = $item['inovasi'];
             $data_indikator_inovasi = $item['indikator_inovasi'];
             $data_upload_inovasi = $item['upload_inovasi'];
+            return response()->json([
+                'status' => true,
+                'message' => "All Data Inovasi!",
+                'data' => $data_inovasi
+            ], 200);
             try {
                 $inovasi = new Inovasi();
                 $inovasi->kode = $data_inovasi['kode'];
