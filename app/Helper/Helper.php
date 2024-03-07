a3da170e00cffde89cf01249ab4903ba00d0419a
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-03-07 20:15:20 +0700

    pushh api

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -112,11 +112,10 @@ class Helper
 			}
 			$nama_file = env('APP_URL').'/'.$folder.'/'.$name.'.'.$file->getClientOriginalExtension();
 			$file->move($folder, $nama_file);
-			dd($file->move($folder, $nama_file));
 			return $nama_file;
 		} catch (\Throwable $th) {
 			$nama_file = "file_error";
-			return $th;
+			return $nama_file;
 		}
 	}
 
