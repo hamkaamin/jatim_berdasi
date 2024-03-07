9d6198f57f8d657844ab36c5917689db071eaab3
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-03-07 09:58:56 +0700

    pushh api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -179,6 +179,7 @@ class ApiController extends Controller
             $data_inovasi = $item['inovasi'];
             $data_indikator_inovasi = $item['indikator_inovasi'];
             $data_upload_inovasi = $item['upload_inovasi'];
+            dd($data_inovasi);
             try {
                 $inovasi = new Inovasi();
                 $inovasi->kode = $data_inovasi['kode'];
