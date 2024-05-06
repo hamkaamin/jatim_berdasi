3442bd270c4a51b568cce69b0c9f4503caa13a53
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-05-06 12:06:39 +0700

    push jember siabang

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -172,11 +172,11 @@ class ApiController extends Controller
 
     public function insert_inovasi2(Request $request)
     {
+        dd($request->all());
         DB::beginTransaction();
 
         try {
             $arr_data = $request->arr_data;
-            dd($request->all());
             $arr_data = json_decode($arr_data, true);
     
             foreach ($arr_data as $item) {
