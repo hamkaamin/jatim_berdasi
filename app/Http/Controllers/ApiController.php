7746019e89e18613608c56308287ce46e4fd3862
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-05-06 12:24:42 +0700

    push jember siabang

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -185,7 +185,7 @@ class ApiController extends Controller
     
                 $inovasi = new Inovasi();
                 // Fill Inovasi attributes
-                $inovasi->fill($data_inovasi);
+                Inovasi::create($data_inovasi);
                 $inovasi->user_id = 34; // Assuming the user ID is fixed
                 $inovasi->save();
     
