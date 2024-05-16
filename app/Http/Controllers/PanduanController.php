2b2d405b5d9e7caabbf3e44bdbd1464e88f71025
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-05-16 15:39:56 +0700

    push inovasi

--- app/Http/Controllers/PanduanController.php
+++ app/Http/Controllers/PanduanController.php
@@ -11,6 +11,7 @@ class PanduanController extends Controller
     public function index()
     {
         $role = Auth::user()->role;
+        dd($role);
         $data = MasterPanduan::where('role',$role)->get();
         return view('panduan.index',compact('data'));
     }
