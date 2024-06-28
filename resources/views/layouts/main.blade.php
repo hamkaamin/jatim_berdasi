976e81ba6ef79d8aeb03d2ce51799426576d1217
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-06-28 15:14:39 +0700

    push test bravo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,6 +23,7 @@
     * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     -->
     @if (env('APP_NAME') == 'BANGKALAN BRAVO')
+        asd
         <link href="{{ asset('admin_asset/main_bravo.css') }}" rel="stylesheet">
     @elseif(env('APP_NAME') == 'JEMBER SIABANG')
         <link href="{{ asset('admin_asset/main_siabang.css') }}" rel="stylesheet">
