6e2df85d546ae91d359e3d5a3e384b1e39788627
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-07-25 18:17:14 +0700

    pull

--- database/seeders/BravoMasterPanduanSeeder.php
+++ database/seeders/BravoMasterPanduanSeeder.php
@@ -16,6 +16,6 @@ class BravoMasterPanduanSeeder extends Seeder
     {
         DB::table('master_panduans')->delete();
         DB::table('master_panduans')->insert(['nama' => 'Manual Book Aplikasi Bravo 2024 (Pengusul)','path'=>'user-manual/bravo/Manual Book Aplikasi Bravo 2024 (Pengusul).pdf','role'=>4]);
-        DB::table('master_panduans')->insert(['nama' => 'Manual Book Aplikasi Bravo 2024 (Superadmin)','path'=>'user-manual/bravo/Manual Book Aplikasi Bravo 2024 (Superadmin).pdf','role'=>1]);DB::table('master_panduans')->insert(['nama' => 'Manual Book Aplikasi Bravo 2024 (Verifikator)','path'=>'user-manual/bravo/Manual Book Aplikasi Bravo 2024 (Verifikator).pdf','role'=>5]);
+        DB::table('master_panduans')->insert(['nama' => 'Manual Book Aplikasi Bravo 2024 (Superadmin)','path'=>'user-manual/bravo/Manual Book Aplikasi Bravo 2024 (Superadmin).pdf','role'=>1]);DB::table('master_panduans')->insert(['nama' => 'Manual Book Aplikasi Bravo 2024 (Verifikator)','path'=>'user-manual/bravo/Manual Book Aplikasi Bravo 2024 (Verifikator).pdf','role'=>2]);
     }
 }
