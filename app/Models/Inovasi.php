3a7f5eb462f416d434f7b77b819d9e0ea76bbdc0
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-07-30 08:57:01 +0700

    push

--- app/Models/Inovasi.php
+++ app/Models/Inovasi.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Inovasi extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $guarded = [];
+    protected $guarded = ['id'];
 
 
     protected static function booted()
