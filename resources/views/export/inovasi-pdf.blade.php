57d6c52e3b434a3d179378886b280ef5eb5b8ab1
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-09-13 14:24:39 +0700

    push

--- resources/views/export/inovasi-pdf.blade.php
+++ resources/views/export/inovasi-pdf.blade.php
@@ -63,7 +63,7 @@
                     $width = '100';
                 @endphp
             @endif
-            <td><img src="http://bravo.egovsuperapp.id/brida-logo.png" width={{ $width }} alt=""></td>
+            <td><img src="https://via.placeholder.com/150" width={{ $width }} alt=""></td>
             <td>
                 @php $nama_app = 'Badan Riset dan Inovasi Daerah (BRIDA)'; @endphp
                 @if (env('APP_NAME') == 'BRAVO BANGKALAN')
