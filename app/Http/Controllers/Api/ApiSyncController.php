47be599a7abfe36a44af07b7d6d9066659eb7b6d
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-09-17 16:00:46 +0700

    push

--- app/Http/Controllers/Api/ApiSyncController.php
+++ app/Http/Controllers/Api/ApiSyncController.php
@@ -39,6 +39,7 @@ class ApiSyncController extends Controller
             }
             // Convert the array to JSON
             $arr_data = json_encode($arr_data);
+            dd($arr_data);
             $response = $client->request('POST', 'https://jatimberdasi.brida.jatimprov.go.id/api/insert_inovasi2', [
                 'headers' => [
                     'Accept' => 'application/json',
