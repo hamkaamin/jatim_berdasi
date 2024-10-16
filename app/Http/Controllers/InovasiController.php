b25d0543fccd5e6f285fcf8c1f147c2d60672e5f
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-10-17 05:16:04 +0700

    push

--- app/Http/Controllers/InovasiController.php
+++ app/Http/Controllers/InovasiController.php
@@ -390,7 +390,7 @@ class InovasiController extends Controller
                 $indikatorData = [];
 
                 // Loop through each indikator and collect the necessary data
-                $indikator_inovasi = DB::table('indikator_inovasi')->where('inovasi_id', $inovasi->kab_integration_id)->get();
+                $indikator_inovasi = DB::table('indikator_inovasi')->where('inovasi_id', $inovasi->id)->get();
                 foreach ($indikator_inovasi as $data) {
                     $indikatorData[] = [
                         'indikator_id' => $data->id,
