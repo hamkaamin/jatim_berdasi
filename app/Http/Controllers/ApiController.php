6e999d40ae85afa3c86da48d18d5ec7758cf5463
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-10-17 05:11:19 +0700

    push

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -320,7 +320,7 @@ class ApiController extends Controller
 
             // update indikator inovasi bobot awal and bobot akhir
             foreach ($request->indikator_data as $data) {
-                dd($data);
+                return($data);
                 DB::table('indikator_inovasi')
                     ->where('kab_inovasi_id', $request->id)
                     ->where('kab_indikator_id', $data['indikator_id'])  
