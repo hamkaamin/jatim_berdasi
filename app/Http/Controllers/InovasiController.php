5976a75885319b6e2b317c11b18d656ac3198823
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-10-17 05:18:49 +0700

    push

--- app/Http/Controllers/InovasiController.php
+++ app/Http/Controllers/InovasiController.php
@@ -410,6 +410,7 @@ class InovasiController extends Controller
                     ], 
                     'verify' => false,
                 ]);
+                dd($indikatorData);
 
                 $responseData = json_decode($response->getBody()->getContents(), true);
                 dd($responseData);
