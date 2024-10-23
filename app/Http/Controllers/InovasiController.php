f9e595c5f92012734d15199836d617ee74d63e26
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2024-10-23 11:50:59 +0700

    push

--- app/Http/Controllers/InovasiController.php
+++ app/Http/Controllers/InovasiController.php
@@ -398,6 +398,7 @@ class InovasiController extends Controller
                         'param_akhir' => $data->param_akhir ?? NULL,  
                     ];
                 }
+                dd($inovasi->integration->url);
                 $response = $client->request('POST', $inovasi->integration->url.'api/kab_status_data_update', [
                     'headers' => [
                         'Accept' => 'application/json',
