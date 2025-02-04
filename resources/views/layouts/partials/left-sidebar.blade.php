0f368642bcebaf0b0c0b978652aad9015766d44e
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2025-02-04 23:22:48 +0700

    Progress migrating to minible

---
+++ resources/views/layouts/partials/left-sidebar.blade.php
@@ -0,0 +1,35 @@
+ <!-- ========== Left Sidebar Start ========== -->
+ <div class="vertical-menu">
+
+     <!-- LOGO -->
+     <div class="navbar-brand-box">
+         <a href="index.html" class="logo logo-dark">
+             <div class="app-header__logo pl-0 pr-5">
+                 <div class="logo-src"></div>
+             </div>
+         </a>
+
+         <a href="index.html" class="logo logo-light">
+             <div class="app-header__logo pl-0 pr-5">
+                 <div class="logo-src"></div>
+             </div>
+         </a>
+     </div>
+
+     <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect vertical-menu-btn">
+         <i class="fa fa-fw fa-bars"></i>
+     </button>
+
+     <div data-simplebar class="sidebar-menu-scroll">
+
+         <!--- Sidemenu -->
+         <div id="sidebar-menu">
+             <!-- Left Menu Start -->
+             <ul class="metismenu list-unstyled" id="side-menu">
+                 @include('layouts.menu')
+             </ul>
+         </div>
+         <!-- Sidebar -->
+     </div>
+ </div>
+ <!-- Left Sidebar End -->
