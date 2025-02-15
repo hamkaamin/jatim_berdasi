37a4d79876d7f0a1d20e05bd2e024fce90b834a6
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2025-02-15 17:17:09 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -252,6 +252,9 @@
             <div id="line_chart_inotek" data-colors='["--bs-success", "--bs-warning","--bs-primary","--bs-danger"]'
                 class="apex-charts" dir="ltr">
             </div>
+            <div id="column_chart_penilaian" data-colors='["--bs-success", "--bs-warning","--bs-primary","--bs-danger"]'
+                class="apex-charts" dir="ltr">
+            </div>
         </div>
         <div class="row">
             @for ($j = 0; $j <= 1; $j++)
@@ -501,6 +504,91 @@
                     }),
                     (chart = new ApexCharts(
                         document.querySelector("#line_chart_inotek"),
+                        options
+                    )).render());
+
+            var BarchartColumnColors = getChartColorsArray("column_chart_penilaian");
+            BarchartColumnColors &&
+                ((options = {
+                        chart: {
+                            height: 350,
+                            type: "bar",
+                            toolbar: {
+                                show: !1
+                            }
+                        },
+                        plotOptions: {
+                            bar: {
+                                horizontal: !1,
+                                columnWidth: "45%",
+                                endingShape: "rounded"
+                            },
+                        },
+                        dataLabels: {
+                            enabled: !1
+                        },
+                        stroke: {
+                            show: !0,
+                            width: 2,
+                            colors: ["transparent"]
+                        },
+                        series: [{
+                                name: "Unggul",
+                                data: [46, 57, 59, 54, 62, 58, 64]
+                            },
+                            {
+                                name: "Cukup",
+                                data: [74, 83, 102, 97, 86, 106, 93]
+                            },
+                            {
+                                name: "Bagus",
+                                data: [37, 42, 38, 26, 47, 50, 54],
+                            },
+                        ],
+                        title: {
+                            text: "Jumlah Penilaian",
+                            align: "left"
+                        },
+                        legend: {
+                            position: "top",
+                            horizontalAlign: "right",
+                            floating: !0,
+                            offsetY: -25,
+                            offsetX: -5,
+                        },
+                        colors: BarchartColumnColors,
+                        xaxis: {
+                            categories: [
+                                "2019",
+                                "2020",
+                                "2021",
+                                "2022",
+                                "2023",
+                                "2024",
+                                "2025",
+                            ],
+                        },
+                        yaxis: {
+                            title: {
+                                text: "Jumlah Penilaian"
+                            }
+                        },
+                        grid: {
+                            borderColor: "#f1f1f1"
+                        },
+                        fill: {
+                            opacity: 1
+                        },
+                        tooltip: {
+                            y: {
+                                formatter: function(e) {
+                                    return "$ " + e + " thousands";
+                                },
+                            },
+                        },
+                    }),
+                    (chart = new ApexCharts(
+                        document.querySelector("#column_chart_penilaian"),
                         options
                     )).render());
         </script>
