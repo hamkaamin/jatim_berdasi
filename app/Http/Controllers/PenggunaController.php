ffb5ec0f887d84d611f5f3057ba6e5d16c1f8fe8
hamdiramadhan <hamdiramadhan22@gmail.com>
2025-02-23 13:21:51 +0700

    user

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -17,72 +17,9 @@ class PenggunaController extends Controller
 {
     public function index(Request $request)
     {
-        set_time_limit(0);
-        $user = Auth::user();
-        $data = User::where('id', '<>', $user->id);
-        if ($user->role != 1 && $user->role != 2) {
-            $data = $data->whereNotIn('role', [1,2]);
-            $arrOpd = [];
-            $opds = [];
-            if ($user->role == 3 || Helper::checkOpd('provinsi', $user)) {
-                $arrKota = [];
-                $idWilayah = $user->role == 3 ? $user->province_id : $user->opd->provinsi_id;
-                $provinsi = Provinsi::findOrFail($idWilayah);
-                $opds = (isset($request->scope) && $request->scope != null && $request->{$request->scope.'_id'} != null) ? Helper::getOpd($request->scope, $request->{$request->scope.'_id'}, $opds) : Helper::getOpd('provinsi', $idWilayah, $opds);
-                foreach ($provinsi->kota()->get() as $kota) {
-                    $arrKota[] = $kota->id;
-                }
-                foreach ($opds as $opd) {
-                    $arrOpd[] = $opd->id;
-                }
-                $data = $data->where(function($query) use ($user, $arrKota, $arrOpd, $idWilayah){
-                    $query->where('maker_id', $user->id)->orWhere('province_id', $idWilayah)->orWhereIn('regency_id', $arrKota)->orWhereIn('opd_id', $arrOpd);
-                });
-            } elseif ($user->role == 4 || Helper::checkOpd('kota', $user)) {
-                $idWilayah = $user->role == 4 ? $user->regency_id : $user->opd->kabkota_id;
-                $opds = (isset($request->scope) && $request->scope != null && $request->{$request->scope.'_id'} != null) ? Helper::getOpd($request->scope, $request->{$request->scope.'_id'}, $opds) : Helper::getOpd('kota', $idWilayah, $opds);
-                foreach ($opds as $opd) {
-                    $arrOpd[] = $opd->id;
-                }
-                $data = $data->where(function($query) use ($user, $arrOpd, $idWilayah){
-                    $query->where('maker_id', $user->id)->orWhere('regency_id', $idWilayah)->orWhereIn('opd_id', $arrOpd);
-                });
-            } elseif (Helper::checkOpd('kecamatan', $user) || Helper::checkOpd('kelurahan', $user)) {
-                $idWilayah = Helper::checkOpd('kecamatan', $user) ? $user->opd->kecamatan_id : $user->opd->kelurahan_id ;
-                $opds = Helper::checkOpd('kecamatan', $user) ? Helper::getOpd('kecamatan', $idWilayah, $opds) : Helper::getOpd('kelurahan', $idWilayah, $opds);
-                $opds = (isset($request->scope) && $request->scope != null && $request->{$request->scope.'_id'} != null) ? Helper::getOpd($request->scope, $request->{$request->scope.'_id'}, $opds) : $opds;
-                foreach ($opds as $opd) {
-                    $arrOpd[] = $opd->id;
-                }
-                $data = $data->where(function($query) use ($user, $arrOpd){
-                    $query->where('maker_id', $user->id)->orWhereIn('opd_id', $arrOpd);
-                });
-            } elseif ($user->role == 6) {
-                $idWilayah = "";
-                if ($user->province_id != null) {
-                    $idWilayah = $user->province_id;
-                    $data = $data->where(function($query) use ($user, $idWilayah){
-                        $query->where('maker_id', $user->id)->orWhere('province_id', $idWilayah);
-                    });
-                } elseif ($user->regency_id != null) {
-                    $idWilayah = $user->regency_id;
-                    $data = $data->where(function($query) use ($user, $idWilayah){
-                        $query->where('maker_id', $user->id)->orWhere('regency_id', $idWilayah);
-                    });
-                } elseif ($user->opd_id != null) {
-                    $idWilayah = $user->opd_id;
-                    $data = $data->where(function($query) use ($user, $idWilayah){
-                        $query->where('maker_id', $user->id)->orWhere('opd_id', $idWilayah);
-                    });
-                }
-
-            }
-        }
-        $role_distinct = $data->pluck('role')->unique();
-        $roles = Role::whereIn('id', $role_distinct)->get();
-        dd($roles, $role_distinct);
-        $data = $data->get(); 
-        return view('daftar-pengguna', compact('data'));
+        set_time_limit(0);  
+        $roles = Role::orderBy('id','desc')->get(); 
+        return view('daftar-pengguna', compact('roles','request'));
     }
 
     public function change_role(Request $request)
--- resources/views/daftar-pengguna.blade.php
+++ resources/views/daftar-pengguna.blade.php
@@ -56,7 +56,94 @@
     @endif
     <div class="row">
         <div class="col">
-            <div class="table-responsive p-3">
+            <ul class="nav nav-tabs">
+                @foreach ($roles as $item)
+                    <li class="nav-item">
+                        <a data-toggle="tab" href="#tab-{{ $item->id }}"
+                            class="{{ $loop->iteration == 1 ? 'active' : '' }} nav-link">
+                            {{ $item->nama }} <span class="badge badge-primary txt_jml_user_{{ $item->id }}"></span>
+                        </a>
+                    </li>
+                @endforeach 
+            </ul>
+            <div class="tab-content">
+                @foreach ($roles as $data)
+                    @php 
+                        $users = $data->get_user($request, $data->id);
+                    @endphp
+                    <div class="tab-pane {{ $loop->iteration == 1 ? 'active' : '' }}" id="tab-{{ $data->id }}"
+                        role="tabpanel">
+                        <div class="table-responsive p-3">
+                            <table class="table align-items-center table-flush datatable">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Nama</th>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Role</th>
+                                        <th style="width: 100px"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->name }}</td>
+                                            <td>{{ $item->username }}</td>
+                                            <td>{{ $item->email }}</td>
+                                            <td>
+                                                {{ Helper::getRole($item->role) }}
+                                                @if ($item->role == 5)
+                                                    @if ($item->opd->provinsi_id != null)
+                                                        - Provinsi
+                                                    @elseif ($item->opd->kabkota_id != null)
+                                                        - Kota
+                                                    @elseif ($item->opd->kecamatan_id != null)
+                                                        - Kecamatan
+                                                    @elseif ($item->opd->kelurahan_id != null)
+                                                        - Kelurahan
+                                                    @endif
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if (in_array(Auth::user()->role, [1, 3, 4]) || (Auth::user()->role == 5 && $item->opd_id == Auth::user()->opd_id))
+                                                    <button data-target="#modalPopup" data-toggle="modal"
+                                                        onclick="modal({{ $item->id }}, 'pengguna')"
+                                                        class="btn m-1 btn-block btn-sm btn-warning"><i
+                                                            class="fa fa-edit"></i>&nbsp;&nbsp;Edit</button>
+                                                    <form style="all: unset"
+                                                        action="{{ route('pengguna.reset-pass', ['id' => $item->id]) }}" method="post">
+                                                        @csrf
+                                                        <button type="submit" class="btn m-1 btn-block btn-sm btn-success"
+                                                            onclick="if(!confirm('Apakah Anda yakin akan me-Reset Passwords pengguna ini ?')){return false;}"><i
+                                                                class="fa fa-key"></i>&nbsp;&nbsp;Reset Pass</button>
+                                                    </form>
+                                                    @if(Auth::user()->id != $item->id)
+                                                    <form style="all: unset"
+                                                        action="{{ route('pengguna.delete', ['id' => $item->id]) }}" method="post">
+                                                        @csrf
+                                                        <button type="submit" class="btn m-1 btn-block btn-sm btn-danger"
+                                                            onclick="if(!confirm('{{ Config::get('delete_confirm') }}')){return false;}"><i
+                                                                class="fa fa-trash-alt"></i>&nbsp;&nbsp;Hapus</button>
+                                                    </form>
+                                                    @endif
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    @push('scripts')
+                    <script>
+                        $('.txt_jml_user_{{ $data->id }}').html('{{ sizeof($users) }}');
+                    </script>
+                    @endpush
+                @endforeach
+            </div>
+            {{-- <div class="table-responsive p-3">
                 <table class="table align-items-center table-flush" id="myTable">
                     <thead class="thead-light">
                         <tr>
@@ -115,17 +202,19 @@
                         @endforeach
                     </tbody>
                 </table>
-            </div>
+            </div> --}}
         </div>
     </div>
 @endsection
 
-@section('script')
-    @include('script.dataTable')
+@section('script') 
     @include('script.modal')
     @include('script.ubahWilayah')
     @include('script.ubahScopeOpd')
     <script>
+        $(document).ready( function () {
+            $('.datatable').DataTable();
+        } );
         function ubahRole(type) {
             $('#role_container').html("<div class=\"text-center my-1\"><h4><b>Loading...</b></h4></div>");
             $.ajax({
