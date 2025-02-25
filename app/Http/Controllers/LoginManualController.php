55ed2e0a1870d1e8db469b28b0457ebefb19b941
hamdiramadhan <hamdiramadhan22@gmail.com>
2025-02-25 16:38:31 +0700

    a

--- app/Http/Controllers/LoginManualController.php
+++ app/Http/Controllers/LoginManualController.php
@@ -11,7 +11,7 @@ class LoginManualController extends Controller
 {
     public function resetallpass()
     {
-        $users = User::whereNotIn('role', [3,4,5,7])->get();
+        $users = User::whereIn('role', [3,4,5,7])->get();
         foreach($users as $u)
         {
             $u->password = Hash::make($u->username);
