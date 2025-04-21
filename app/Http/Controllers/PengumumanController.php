b2389851171c0898a1f454ec919e4eccca27b6b0
salehattamimi <68505552+salehattamimi@users.noreply.github.com>
2025-04-21 13:23:48 +0700

    fix: gap save code

--- app/Http/Controllers/PengumumanController.php
+++ app/Http/Controllers/PengumumanController.php
@@ -19,46 +19,46 @@ class PengumumanController extends Controller
 
     public function save(Request $request)
     {
-            if ($request->id == 0) {
-                $data = new Pengumuman;
-            } else {
-                $data = Pengumuman::findOrFail($request->id);
-            }
-            try {
-                DB::beginTransaction();
-            
-                $data->judul = $request->judul;
-                $data->deskripsi = $request->deskripsi;
-                $data->is_aktif = $request->is_aktif;
-                $data->save();
-            
-                if ($request->hasFile('file')) {
-                    $nama_file = Helper::save_file(
-                        $request->file('file'),
-                        uniqid(),
-                        'file_pengumuman',
-                        $data->file,
-                        ['pdf', 'jpg', 'jpeg', 'png']
-                    );
-            
-                    if ($nama_file['valid'] == false) {
-                        DB::rollBack();
-                        return redirect()->back()->with('error', $nama_file['message']);
-                    } else {
-                        $data->file = $nama_file['file_name'];
-                        $data->save();
-                    }
+        if ($request->id == 0) {
+            $data = new Pengumuman;
+        } else {
+            $data = Pengumuman::findOrFail($request->id);
+        }
+        try {
+            DB::beginTransaction();
+        
+            $data->judul = $request->judul;
+            $data->deskripsi = $request->deskripsi;
+            $data->is_aktif = $request->is_aktif;
+            $data->save();
+        
+            if ($request->hasFile('file')) {
+                $nama_file = Helper::save_file(
+                    $request->file('file'),
+                    uniqid(),
+                    'file_pengumuman',
+                    $data->file,
+                    ['pdf', 'jpg', 'jpeg', 'png']
+                );
+        
+                if ($nama_file['valid'] == false) {
+                    DB::rollBack();
+                    return redirect()->back()->with('error', $nama_file['message']);
+                } else {
+                    $data->file = $nama_file['file_name'];
+                    $data->save();
                 }
-            
-                DB::commit();
-                return redirect()->back()->with('success', Config::get('save_success'));
-            
-            } catch (\Throwable $th) {
-                DB::rollBack();
-                // Log error kalau perlu
-                // Log::error($th->getMessage());
-                return redirect()->back()->with('error', 'Terjadi kesalahan saat menyimpan data.');
             }
+        
+            DB::commit();
+            return redirect()->back()->with('success', Config::get('save_success'));
+        
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            // Log error kalau perlu
+            // Log::error($th->getMessage());
+            return redirect()->back()->with('error', 'Terjadi kesalahan saat menyimpan data.');
+        }
     }
 
     public function delete(Request $request)
